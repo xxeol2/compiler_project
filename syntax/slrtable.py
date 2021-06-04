0255b8dcb1c65d17daad2b615736ad8b06f2f3d9
xxeol2 <71129059+xxeol2@users.noreply.github.com>
2021-06-04 22:15:32 +0900

    [ADD] CFG.txt

---
+++ syntax/slrtable.py
@@ -0,0 +1,110 @@
+slrTable = [
+    # STATE 0
+    {
+        "vtype" : "s5",
+        "class" : "s6",
+        "$" : "r4",
+        "CODE" : "1",
+        "VDECL" : "2",
+        "FDECL" : "3"
+    },
+    # STATE 1 
+
+    # STATE 2 
+    {
+        "vtype": "s5"
+    },
+    # STATE 3
+    {
+        "vtype" : "s5"
+    },
+    # STATE 4 
+    {
+        "vtype" : "s5"
+    },
+    # STATE 5 
+    # STATE 6 
+    # STATE 7 
+    # STATE 8 
+    # STATE 9 
+    # STATE 10
+    # STATE 11
+    # STATE 12
+    # STATE 13
+    {
+        "vtype": "r5"
+    },
+    # STATE 14
+    # STATE 15
+    # STATE 16
+    # STATE 17
+    # STATE 18
+    # STATE 19
+    # STATE 20
+    # STATE 21
+    # STATE 22
+    # STATE 23
+    # STATE 24
+    # STATE 25
+    # STATE 26
+    # STATE 27
+    # STATE 28
+    # STATE 29
+    # STATE 30
+    # STATE 31
+    # STATE 32
+    # STATE 33
+    # STATE 34
+    # STATE 35
+    # STATE 36
+    # STATE 37
+    # STATE 38
+    # STATE 39
+    # STATE 40
+    # STATE 41
+    # STATE 42
+    # STATE 43
+    # STATE 44
+    # STATE 45
+    # STATE 46
+    # STATE 47
+    # STATE 48
+    # STATE 49
+    # STATE 50
+    # STATE 51
+    # STATE 52
+    # STATE 53
+    # STATE 54
+    # STATE 55
+    # STATE 56
+    # STATE 57
+    # STATE 58
+    # STATE 59
+    # STATE 60
+    # STATE 61
+    # STATE 62
+    # STATE 63
+    # STATE 64
+    # STATE 65
+    # STATE 66
+    # STATE 67
+    # STATE 68
+    # STATE 69
+    # STATE 70
+    # STATE 71
+    # STATE 72
+    # STATE 73
+    # STATE 74
+    # STATE 75
+    # STATE 76
+    # STATE 77
+    # STATE 78
+    # STATE 79
+    # STATE 80
+    # STATE 81
+    # STATE 82
+    # STATE 83
+    # STATE 84
+    # STATE 85
+    # STATE 86
+]
